f6be97f2f20098c768880092077f786c6db1571c
VladProgrammer08 <bumbuv136@gmail.com>
2024-04-12 20:58:28 -0700

    Created a Fish Model with it's properties

---
+++ FishStoreApplication/Models/Fish.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FishStoreApplication.Models
+{
+    /// <summary>
+    /// Represents a single fish for available for purchase
+    /// </summary>
+    public class Fish
+    {
+        /// <summary>
+        /// The unique identifier for each fish product
+        /// (Primary Key)
+        /// </summary>
+        [Key]
+        public int FishId { get; set; }
+        /// <summary>
+        /// Represents the name of the fish breed.
+        /// </summary>
+        [Required]
+        public string BreedName { get; set; }
+
+        /// <summary>
+        /// Represents the price of the fish.
+        /// </summary>
+        [Range(0, int.MaxValue)]
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Represents a description of the fish.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
