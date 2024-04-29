2080651b6c90638d97a39627609fd25b58acefb2
VladProgrammer08 <bumbuv136@gmail.com>
2024-04-28 17:29:16 -0700

    All new users get a Customer role

--- FishStoreApplication/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FishStoreApplication/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using FishStoreApplication.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -124,6 +125,9 @@ namespace FishStoreApplication.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    // Adds new accounts to the customer role
+                    await _userManager.AddToRoleAsync(user, IdentityHelper.Customer);
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
