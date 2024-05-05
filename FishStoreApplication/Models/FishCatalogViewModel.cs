87366031e7cffaab32d893c5b74d381b6c967dea
VladProgrammer08 <bumbuv136@gmail.com>
2024-05-04 22:18:50 -0700

    Created FishCatalogModel for pagination

---
+++ FishStoreApplication/Models/FishCatalogViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FishStoreApplication.Models
+{
+    public class FishCatalogViewModel
+    {
+        public FishCatalogViewModel(List<Fish> fishes, int lastPage, int currPage)
+        {
+            Fishes = fishes;
+            LastPage = lastPage;
+            CurrentPage = currPage;
+        }
+
+        public List<Fish> Fishes { get; set; }
+        
+        public int LastPage { get; set; }
+
+        public int CurrentPage { get; set; }
+    }
+}
