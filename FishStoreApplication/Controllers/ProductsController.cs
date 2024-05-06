98ff3f91b96905b18bd0b2994c0f0ef219c3fbf4
VladProgrammer08 <bumbuv136@gmail.com>
2024-05-05 18:24:17 -0700

    Added Bootstrap 5 cards to display fish information

--- FishStoreApplication/Controllers/ProductsController.cs
+++ FishStoreApplication/Controllers/ProductsController.cs
@@ -1,18 +1,37 @@
 ﻿using FishStoreApplication.Data;
+using FishStoreApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FishStoreApplication.Controllers
 {
     public class ProductsController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        public ProductsController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+		private readonly ApplicationDbContext _context;
+		public ProductsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<IActionResult> Index(int? id)
+		{
+			const int NumFishToDisplayPerPage = 6;
+			const int PageOffSet = 1;
+			int currPage = id ?? 1;
+
+			int totalNumOfProducts = await _context.Fishes.CountAsync();
+			double maxNumPages = Math.Ceiling((double)totalNumOfProducts / NumFishToDisplayPerPage);
+			int lastPage = Convert.ToInt32(maxNumPages);
+
+			
+			List<Fish> fishes = await (from fish in _context.Fishes
+									   select fish)
+									   .Skip(NumFishToDisplayPerPage * (currPage - PageOffSet))
+									   .Take(NumFishToDisplayPerPage)
+									   .ToListAsync();
+			FishCatalogViewModel catalogModel = new(fishes, lastPage, currPage);
+
+			return View(catalogModel);
+		}
+
+	}
 }
