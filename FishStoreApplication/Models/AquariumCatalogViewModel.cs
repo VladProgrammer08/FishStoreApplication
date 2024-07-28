7408565749e3ab0674d27ca9fc1013e3277207de
VladProgrammer08 <bumbuv136@gmail.com>
2024-07-27 20:02:57 -0700

    Created a model for pagination

---
+++ FishStoreApplication/Models/AquariumCatalogViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FishStoreApplication.Models
+{
+    public class AquariumCatalogViewModel
+    {
+        public AquariumCatalogViewModel(List<Aquarium> aquariums, int lastPage, int currPage)
+        {
+            Aquariums = aquariums;
+            LastPage = lastPage;
+            CurrentPage = currPage;
+        }
+
+        public List<Aquarium> Aquariums { get; set; }
+
+        public int LastPage { get; set; }
+
+        public int CurrentPage { get; set; }
+    }
+}
