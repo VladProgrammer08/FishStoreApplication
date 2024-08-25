8374a83dc3299caf30c2063eb381ba26b2dda4b4
VladProgrammer08 <bumbuv136@gmail.com>
2024-08-24 18:36:47 -0700

    Created an Email body as a long string

--- FishStoreApplication/Controllers/CartController.cs
+++ FishStoreApplication/Controllers/CartController.cs
@@ -1,6 +1,6 @@
 ﻿using FishStoreApplication.Data;
 using FishStoreApplication.Models;
-using FishStoreApplication.Services;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,8 +11,10 @@ using Newtonsoft.Json;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.Configuration;
+using System.Drawing;
 using System.Net.Mail;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace FishStoreApplication.Controllers
@@ -22,15 +24,13 @@ namespace FishStoreApplication.Controllers
         private readonly ApplicationDbContext _context;
         private const string Cart = "ShopingCart";
         private readonly IConfiguration _configuration;
-        private readonly IRazorViewToStringRenderer _viewRenderer;
 
 
 
-        public CartController(ApplicationDbContext context, IConfiguration configuration, IRazorViewToStringRenderer viewRenderer)
+        public CartController(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
             _configuration = configuration;
-            _viewRenderer = viewRenderer;;
         }
         public IActionResult Add(int id)
         {
@@ -189,7 +189,7 @@ namespace FishStoreApplication.Controllers
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var cart = _context.Carts.Include(c => c.Items)
                                      .FirstOrDefault(c => c.UserId == userId);
-
+            string userName = User.Identity.Name;
             if (cart != null && cart.Items.Any())
             {
                 // Get the user's email address
@@ -211,9 +211,78 @@ namespace FishStoreApplication.Controllers
                     TotalPrice = cartItems.Sum(ci => ci.Product.Price * ci.Quantity),
                     TaxRate = 0.10
                 };
-                var emailBody = await _viewRenderer.RenderViewToStringAsync("EmailTemplate", model);
+                var emailBody = new StringBuilder();
+                emailBody.AppendLine("<div style=\"width: 40%; margin: 0 auto; border: 1px solid #ccc; padding: 30px; border-radius: 5px;\"\">");
+                emailBody.AppendLine("<h2 style=\"text-align: center; color: #0074D9;\">Aqua World</h2>");
+                emailBody.AppendLine("<h1 style=\"text-align: center; font-weight: bold; color: black;\">Order Confirmation</h1>");
+                emailBody.AppendLine($"<p style=\"text-align: center; font-size: 15px; font-weight: bold; color: black;\">Hello,{userName}. We received your order and we'll let you know when we ship it out</p>");
+                emailBody.AppendLine("<hr style=\"border: 1px solid #ccc;\" />");;
+                emailBody.AppendLine($"<p style=\"font-size: 15px; font-weight: bold; color: black;\">Your Order (#XXXXXXX):</p>");
+                emailBody.AppendLine("<hr style=\"border: 1px solid #ccc;\" />");
+                emailBody.AppendLine("<table style=\"width: 100%; border-collapse: collapse;\">");
 
-                // Send the email
+                bool isEvenRow = false;
+
+                foreach (var item in model.Items)
+                {
+                    string rowColor = isEvenRow ? "#f2f2f2" : "#ffffff";
+                    isEvenRow = !isEvenRow;
+
+                    emailBody.AppendLine("<tr>");
+                    emailBody.AppendLine($"<td style=\"padding: 8px; color: black; background-color: {rowColor};\">{item.Quantity} X {item.Product.Name}</td>");
+                    emailBody.AppendLine($"<td style=\"padding: 8px; color: black; background-color: {rowColor};\">{item.Product.Price:C}</td>");
+                    emailBody.AppendLine("</tr>");
+                }
+                emailBody.AppendLine("</table>");
+                emailBody.AppendLine("<hr style=\"border: 1px solid #ccc;\" />");
+
+                // 2nd table
+                emailBody.AppendLine("<table style=\"width: 100%; border-collapse: collapse;\">");
+                emailBody.AppendLine("<tr>");
+                emailBody.AppendLine("<td style=\"vertical-align: top;\">");
+                // Shipping
+                emailBody.AppendLine("<div>");
+                emailBody.AppendLine("<h3 style=\"font-weight: bold; color: black;\">Shipping Address</h3>");
+                emailBody.AppendLine($"<p style=\"margin-bottom: 0;\">{userEmailClaim}</p>");
+                emailBody.AppendLine("<p style=\"margin-bottom: 0; margin-top: 0; color: black;\">XXXX X XXXX XXX,</p>");
+                emailBody.AppendLine("<p style=\"margin-top: 0; color: black;\">XXXXXX XX XXXXXX</p>");
+                emailBody.AppendLine("<h3 style=\"font-weight: bold; color: black;\">Payment</h3>");
+                emailBody.AppendLine("<p style=\"color: black;\">Capital One - XXXX XXXX</p>");
+                emailBody.AppendLine("</div>");
+                emailBody.AppendLine("</td>");
+
+                emailBody.AppendLine("<td style=\"vertical-align: top; width:20%;\">");
+                // Details
+                emailBody.AppendLine("<div>");
+                emailBody.AppendLine("<h3 style=\"font-weight: bold; color: black;\">Details</h3>");
+                emailBody.AppendLine($"<p style=\"font-weight: bold; color: black;\">Total Price:</p>");
+                emailBody.AppendLine($"<p style=\"font-weight: bold; color: black;\">Tax:</p>");
+                emailBody.AppendLine("</div>");
+                emailBody.AppendLine("</td>");
+
+                // Price
+                emailBody.AppendLine("<td style=\"vertical-align: top;\">");
+                emailBody.AppendLine("<div>");
+                emailBody.AppendLine("<br />");
+                emailBody.AppendLine("<br />");
+                emailBody.AppendLine($"<p style=\"margin-bottom: 17; margin-top: 14; color: black;\">${model.TotalPrice:0.00}</p>");
+                emailBody.AppendLine($"<p style=\"color: black;\">${model.TotalTax:0.00}</p>");
+                emailBody.AppendLine("</div>");
+                emailBody.AppendLine("</td>");
+                emailBody.AppendLine("</tr>");
+                emailBody.AppendLine("</table>");
+                emailBody.AppendLine("<div style=\"clear: both;\"></div>");
+
+                emailBody.AppendLine("<hr style=\"border: 1px solid #ccc;\" />");
+               
+                emailBody.AppendLine($"<p style=\"text-align: center; font-size: 13px; margin-bottom: 0; font-weight: bold; color: black;\">Order Total</p>");
+                emailBody.AppendLine($"<p style=\"text-align: center; font-size: 30px; font-weight: bold; margin-top: 0; color: black;\">${model.FinalPrice.ToString("0.00")}</p>");
+                emailBody.AppendLine("<hr style=\"border: 1px solid #ccc;\" />");
+                emailBody.AppendLine("<p style=\"text-align: center; font-weight: bold; font-size: 21px; margin-bottom: 12; color: black;\">Keep in Touch");
+                emailBody.AppendLine($"<p style=\"text-align: center; font-weight: bold; color: black;\">If you have any questions, concerns, or suggestions,<br /> please do not reply to this email. </p>");
+                emailBody.AppendLine("</div>");
+
+
                 var apiKey = _configuration["ApiKey"];
                 var fromEmail = _configuration["FromEmail"];
 
@@ -222,13 +291,12 @@ namespace FishStoreApplication.Controllers
                 {
                     From = new EmailAddress(fromEmail),
                     Subject = "Order Receipt",
-                    HtmlContent = emailBody // Rendered HTML content
+                    HtmlContent = emailBody.ToString(),
                 };
                 msg.AddTo(new EmailAddress(userEmailClaim));
 
                 await client.SendEmailAsync(msg);
 
-                // Remove cart items
                 _context.CartItems.RemoveRange(cart.Items);
                 _context.SaveChanges();
 
